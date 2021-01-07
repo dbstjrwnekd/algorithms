297792fc7d7fd5cd54e18ea2f1eafa933d61d213
dbstjrwnekd <sh4869@ajou.ac.kr>
2021-01-07 15:16:22 +0900

    LeetCode 78. Subsets

---
+++ leetcode78_subsets.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        answer = []
+        def dfs(index: int, discovered: List[int]):
+            answer.append(discovered[:])
+            if len(discovered) == len(nums):
+                return
+            for i in range(index,len(nums)):
+                discovered.append(nums[i])
+                dfs(i+1,discovered)
+                discovered.pop()
+        dfs(0,[])
+        return answer
