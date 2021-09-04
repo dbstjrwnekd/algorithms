efe7057c2d75e59cc7bb891ed963f14ac13d7aaf
dbstjrwnekd <sh4869@ajou.ac.kr>
2021-09-04 11:22:55 +0900

    LeetCode 347 Top-K frequent elements

---
+++ leetcode_347_top_k_frequent_elements.py
@@ -0,0 +1,6 @@
+import collections
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        return list(zip(*collections.Counter(nums).most_common(k)))[0]
+        
