0b68f55cf7d1ff03393a1b8163387b03f594d215
dbstjrwnekd <sh4869@ajou.ac.kr>
2021-09-06 23:15:42 +0900

    LeetCode 543 Diameter of Binary Tree2

--- leetcode_543_Diameter_of_Binary_Tree.py
+++ leetcode_543_Diameter_of_Binary_Tree.py
@@ -6,20 +6,14 @@
 #         self.right = right
 class Solution:
     maxDepth = 0
-    def dfs(self, node):
-        if not node:
-            return 0
-            
-        return max(self.dfs(node.left), self.dfs(node.right)) + 1
-    
     def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        if not root:
-            return
-        
-        self.diameterOfBinaryTree(root.left)
-        self.diameterOfBinaryTree(root.right)
-        self.maxDepth = max(self.maxDepth, self.dfs(root.left)+self.dfs(root.right))
-        
-        
+        def dfs(node):
+            if not node:
+                return -1
+            left = dfs(node.left)
+            right = dfs(node.right)
+            self.maxDepth = max(self.maxDepth, left+right+2)
+            return max(left, right) + 1
+        dfs(root)
         return self.maxDepth
             
