9e2c957d7ab845039aaddfea6f8acbc6d236f69b
dbstjrwnekd <sh4869@ajou.ac.kr>
2021-10-04 10:52:41 +0900

    2018 Kakao Blind recruitment Dart Game

---
+++ 2018_kakao_blind_recruitment_dart_game.java
@@ -0,0 +1,64 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+
+class Solution {
+    public int solution(String dartResult) {
+        HashMap<Character, Integer> SDT = this.getDefaultSDT();
+        int answer = 0;
+        int step = 0;
+        boolean fin = false;
+        String before = "";
+        String current = "";
+        while (step < dartResult.length()) {
+            char ch = dartResult.charAt(step);
+            if (this.isDisit(ch)) {
+                if (fin) {
+                    answer += Integer.parseInt(current);
+                    before = current;
+                    current = "";
+                    fin = false;
+                }
+                current += ch;
+            } else {
+                if (SDT.containsKey(ch)) {
+                    current = Integer.toString((int) Math.pow(Integer.parseInt(current), SDT.get(ch)));
+                    fin = true;
+                } else {
+                    int next = 0;
+                    if (ch == '*') {
+                        next = Integer.parseInt(current) * 2;
+                        answer += next;
+                        if (before != "")
+                            answer += Integer.parseInt(before);
+                    } else {
+                        next = -1 * Integer.parseInt(current);
+                        answer += next;
+                    }
+                    before = Integer.toString(next);
+                    current = "";
+                    fin = false;
+                }
+            }
+            step++;
+        }
+
+        if (fin)
+            answer += Integer.parseInt(current);
+
+        return answer;
+    }
+
+    public HashMap<Character, Integer> getDefaultSDT() {
+        HashMap<Character, Integer> defaultSDT = new HashMap<Character, Integer>();
+        defaultSDT.put('S', 1);
+        defaultSDT.put('D', 2);
+        defaultSDT.put('T', 3);
+        return defaultSDT;
+    }
+
+    public boolean isDisit(char ch) {
+        if ('0' <= ch && ch <= '9')
+            return true;
+        return false;
+    }
+}
